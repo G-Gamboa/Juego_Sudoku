b99e9cb1fb8193ebbb2c777d21955a04ab9ebf29
Guillermo Gamboa <gamboaguillermo12@gmail.com>
2022-04-30 16:13:32 -0600

    Logica para resolver un sudoku preestablecido utilizando clases

--- logica_Sudoku.py
+++ logica_Sudoku.py
@@ -1,83 +1,89 @@
+class LOG_SUDOKU():
+    def __init__(self):
+        pass
 
-TAMAÑO  =  9 
+    def crear(self):
+        self.TAMAÑO=9
+        self.base=[ 
+            [ 6 , 5 , 0 , 8 , 7 , 3 , 0 , 9 , 0 ], 
+            [ 0 , 0 , 3 , 2 , 5 , 0 , 0 , 0 , 8 ], 
+            [ 9 , 8 , 0 , 1 , 0 ,4 , 3 , 5 , 7 ], 
+            [ 1 , 0 , 5 , 0 , 0 , 0 , 0 , 0 , 0 ], 
+            [ 4 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2 ], 
+            [ 0 , 0 , 0 , 0 , 0 , 0 , 5 ,0 , 3 ], 
+            [ 5 , 7 , 8 , 3 , 0 , 1 , 0 , 2 , 6 ], 
+            [ 2 , 0 , 0 , 0 , 4 , 8 , 9 , 0 , 0 ], 
+            [ 0 , 9 , 0 , 6 , 2 , 5 , 0 , 8 , 1 ]]
 
-#Creamos una matriz de ejemplo para resolver.
-base  =  [ 
-    [ 6 , 5 , 0 , 8 , 7 , 3 , 0 , 9 , 0 ], 
-    [ 0 , 0 , 3 , 2 , 5 , 0 , 0 , 0 , 8 ], 
-    [ 9 , 8 , 0 , 1 , 0 ,4 , 3 , 5 , 7 ], 
-    [ 1 , 0 , 5 , 0 , 0 , 0 , 0 , 0 , 0 ], 
-    [ 4 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2 ], 
-    [ 0 , 0 , 0 , 0 , 0 , 0 , 5 ,0 , 3 ], 
-    [ 5 , 7 , 8 , 3 , 0 , 1 , 0 , 2 , 6 ], 
-    [ 2 , 0 , 0 , 0 , 4 , 8 , 9 , 0 , 0 ], 
-    [ 0 , 9 , 0 , 6 , 2 , 5 , 0 , 8 , 1 ]]
+        
+    def  impresion_sudoku (self): 
+        for  i  in  self.base : 
+            print  ( i )
 
-#Coloca el sudoku de línea en línea
-def  impresion_sudoku (): 
-    for  i  in  base : 
-        print  ( i )
+    def  asignacion (self, fila , columna ): 
+        asignacion_numeros  =  0 
+        for  i  in range ( 0 , self.TAMAÑO ): 
+            for j  in range  ( 0 , self.TAMAÑO ): 
+                
+                #Si hay un espacio en blanco acá lo detecta
+                if self.base [ i ] [ j ]  ==  0 : 
+                    fila =  i 
+                    columna  =  j 
+                    asignacion_numeros  =  1 
+                    a  =  [ fila ,  columna ,  asignacion_numeros ] 
+                    return  a 
+        a  =  [ - 1 ,  - 1 ,  asignacion_numeros ] 
+        return  a 
 
-#Asigna valores a los espacios que se encuentran en blanco
-def  asignacion ( fila ,  columna ): 
-    asignacion_numeros  =  0 
-    for  i  in range ( 0 , TAMAÑO ): 
-        for j  in range  ( 0 , TAMAÑO ): 
-            
-            #Si hay un espacio en blanco acá lo detecta
-            if base [ i ] [ j ]  ==  0 : 
-                fila =  i 
-                columna  =  j 
-                asignacion_numeros  =  1 
-                a  =  [ fila ,  columna ,  asignacion_numeros ] 
-                return  a 
-    a  =  [ - 1 ,  - 1 ,  asignacion_numeros ] 
-    return  a 
-  
-#Comprueba que el valor n no se repita
-def  comprobacion ( n ,  r ,  c ): 
-    #Comprueba la fila
-    for  i  in range ( 0 ,TAMAÑO ): 
-        #Si hay una celda con el mismo valor retorna Falso 
-        if base [ r ] [ i ]  ==  n : 
-            return  False 
-    #Comprueba las columnas
-    for  i  in range ( 0 , TAMAÑO ): 
-        #Si hay una celda con el mismo valor retorna Falso 
-        if  base [ i ] [ c ]  ==  n : 
-            return  False 
-    
-    sub_fila  =  ( r // 3 ) * 3 
-    sub_columna  =  ( c// 3 ) * 3 ; 
-    #Comprueba que los valores no se repitan en los espacios 3x3
-    for  i  in range ( sub_fila , sub_fila + 3 ): 
-        for  j  in range ( sub_columna , sub_columna + 3 ): 
-            if  base [ i ] [ j ] == n : 
+
+    def  comprobacion (self, n ,  r ,  c ): 
+        #Comprueba la fila
+        for  i  in range ( 0 , self.TAMAÑO ): 
+            #Si hay una celda con el mismo valor retorna Falso 
+            if self.base [ r ] [ i ]  ==  n : 
                 return  False 
-    return  True
+        #Comprueba las columnas
+        for  i  in range ( 0 , self.TAMAÑO ): 
+            #Si hay una celda con el mismo valor retorna Falso 
+            if  self.base [ i ] [ c ]  ==  n : 
+                return  False 
 
-#Resolución del sudokus
-def  solucion_sudoku (): 
-    fil  =  0 
-    col  =  0 
-    #SI en este punto cada punto tiene un valor asignado significa que pasó todas las comprobaciones
-    a  =  asignacion ( fil ,  col ) 
-    if  a [ 2 ]  ==  0 : 
-        return  True 
-    fil  =  a [ 0 ] 
-    col  =  a [ 1 ]
-  
-    for  i  in range ( 1 , 10 ): 
-        if comprobacion ( i ,  fil ,  col ): 
-            base [ fil ] [ col ]  =  i 
-            #regreso 
-            if  solucion_sudoku (): 
-                return  True 
-            base [ fil ] [ col ] = 0
-    return  False
-if  solucion_sudoku (): 
-    impresion_sudoku () 
+        sub_fila  =  ( r // 3 ) * 3 
+        sub_columna  =  ( c// 3 ) * 3 ; 
+        #Comprueba que los valores no se repitan en los espacios 3x3
+        for  i  in range ( sub_fila , sub_fila + 3 ): 
+            for  j  in range ( sub_columna , sub_columna + 3 ): 
+                if  self.base [ i ] [ j ] == n : 
+                    return  False 
+        return  True
+
+
+    def  solucion_sudoku (self): 
+        fil  =  0 
+        col  =  0 
+        #SI en este punto cada punto tiene un valor asignado significa que pasó todas las comprobaciones
+        a  =  self.asignacion ( fil ,  col ) 
+        if  a [ 2 ]  ==  0 : 
+            return  True 
+        fil  =  a [ 0 ] 
+        col  =  a [ 1 ]
+    
+        for  i  in range ( 1 , 10 ): 
+            if self.comprobacion ( i ,  fil ,  col ): 
+                self.base [ fil ] [ col ]  =  i 
+                #regreso 
+                if  self.solucion_sudoku(): 
+                    return  True 
+                self.base [ fil ] [ col ] = 0
+        return  False
+
+#-----------------------------PARTE CENTRAL---------------------------
+
+pruebas=LOG_SUDOKU()
+pruebas.crear()
+pruebas.impresion_sudoku ()
+print("------------------------------------------------------")
+if  pruebas.solucion_sudoku (): 
+    pruebas.impresion_sudoku () 
 else : 
     print ( "Sin solución" )
-
-
--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,7 @@
 #Documento que servirá para subir las pruebas realizadas
+import logica_Sudoku
+
+a=logica_Sudoku.LOG_SUDOKU()
+
+a.crear()
+a.impresion_sudoku()
